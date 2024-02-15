a748b6fab6c9a8839accef531120441bd361765d
Shazzad01 <shazzadm065@gmail.com>
2024-02-15 14:44:13 +0600

    Day02/15/2024

--- src/test/java/LocatorLearning/TC01testNg.java
+++ src/test/java/LocatorLearning/TC01testNg.java
@@ -1,40 +1,50 @@
 package LocatorLearning;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.*;
+import utilities.DriverSetup;
 
-public class TC01testNg {
+public class TC01testNg extends DriverSetup {
     @Test(priority = 1)
-    public void testcase1(){
+    public void testcase1() {
         System.out.println("Normal test case1 ");
     }
 
     @Test(priority = 0)
-    public void testcase2(){
+    public void testcase2() {
         System.out.println("Normal test case2 ");
     }
 
     @BeforeMethod
-    public void beforeMathod(){
+    public void beforeMathod() {
         System.out.println("execute before every mathod");
     }
-    @AfterMethod
-    public void afterMathod(){
-        System.out.println("execute after every mathod");
-    }
+
     @BeforeSuite
-    public void beforeSuit(){
+    public void beforeSuit() {
         System.out.println("execute before every suit");
     }
+
     @BeforeTest
-    public void beforeTest(){
+    public void beforeTest() {
         System.out.println("execute before every test");
     }
+
     @BeforeClass
-    public void beforeClass(){
+    public void beforeClass() {
         System.out.println("execute before every class");
     }
+
     @BeforeGroups
-    public void beforeGroups(){
+    public void beforeGroups() {
         System.out.println("execute before every group");
+    }
+
+    @Test
+
+    public void clickDarazLogin() throws InterruptedException {
+        driver.get("https://www.daraz.com.bd/");
+        driver.findElement(By.className("bld-txt")).click();
+        Thread.sleep(5000);
     }
 }
